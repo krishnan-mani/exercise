870e5bb8a8db430767b7ca60ad7f6436f4f0cc66
Krishnan Mani <kmani@equalexperts.com>
2014-06-07 11:40:02 +0530

    Trivial improvements

--- print_temperature_spread.rb
+++ print_temperature_spread.rb
@@ -10,14 +10,14 @@ class WeatherInfo
   end
 
   def to_s
-    "weather on day #{@day} highest temperature #{max_temp}, lowest temperature #{min_temp}"
+    "weather on day #{day} highest temperature #{max_temp}, lowest temperature #{min_temp}"
   end
 
   def <=>(other)
-    (other.max_temp - other.min_temp) <=> (@max_temp - @min_temp)
+    other.spread <=> spread
   end
 
-  def spread; @max_temp - @min_temp; end
+  def spread; max_temp - min_temp; end
 end
 
 weather_data_parsed = []
@@ -31,12 +31,13 @@ end
 valid_data = weather_data_parsed.slice(2, weather_data_parsed.length)
 
 highest_spread = valid_data.sort.last
-print("Least temperature spread on day #{highest_spread.day} has least spread #{highest_spread.spread}")
+print "Least temperature spread on day #{highest_spread.day} has least spread #{highest_spread.spread}"
 print "\n"
 
 coldest_day = (valid_data.sort { |a, b| a.min_temp <=> b.min_temp })[0]
-print("Coldest day #{coldest_day.day}, temperature dropped to  #{coldest_day.min_temp}")
+print "Coldest day #{coldest_day.day}, temperature dropped to  #{coldest_day.min_temp}"
 print "\n"
 
 hottest_day = (valid_data.sort { |a, b| b.max_temp <=> a.max_temp })[0]
-print("Hottest day #{hottest_day.day}, mercury soared to #{hottest_day.max_temp}")
+print "Hottest day #{hottest_day.day}, mercury soared to #{hottest_day.max_temp}"
+print "\n"
